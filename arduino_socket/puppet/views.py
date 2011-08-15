e3246f38b6c9dd8d4d558ee6c1d50c5a517e2209
= <ajfisher.td@gmail.com>
2011-08-15 21:53:55 +1000

    added all the socket code and messaging for that.

--- arduino_socket/puppet/views.py
+++ arduino_socket/puppet/views.py
@@ -1,12 +1,51 @@
+import serial
+
 from django.shortcuts import get_object_or_404, render, redirect
 from django_socketio import events
+
+SERIAL_INTERFACE = "/dev/ttyUSB0"
+SERIAL_BAUD = 115200
+
+try:
+    ser = serial.Serial(SERIAL_INTERFACE, SERIAL_BAUD, timeout=60)
+except:
+    print "Can't get a serial connection"
+
 
 @events.on_message(channel="^puppet")
 def message(request, socket, message):
     message = message[0]
     if message["action"] == "movement":
-        print "x: %s y: %s z: %s" % (message["x"], message["y"], message["z"])
         socket.send({"action": "ack"})
+        
+        # pick up the values from the socket message
+        x = int(message["x"])
+        y = int(message["y"])
+        z = int(message["z"])
+        
+        # now normalise the values as needed
+        if message["method"] == "orientation":
+            # put the vals back into +ive integer range 0-180
+            x += 90
+            y += 90
+            
+        if x > 180:
+            x = 180
+        if y > 180:
+            y = 180
+        if z >= 360:
+            z = 0
+
+        # work out the z bytes
+        zh = z >> 8
+        if z > 255 :
+            zl = z - 256
+        else:
+            zl = z
+
+        #print "x: %s y: %s z: %s" % (x, y, z)
+        ser.write("%s%s%s%s%s%s" % (chr(255), chr(255), chr(x), chr(y), chr(zh), chr(zl)))
+        
     elif message["action"] == "test":
         # this is a test of the socket
         print "Test of the socket"
--- arduino_socket/static/js/drive.js
+++ arduino_socket/static/js/drive.js
@@ -1,26 +1,32 @@
 var socket;
 var room = "puppet";
+var z = 250;  // use this as a dummy for FF
 
 // use this for Firefox
 window.addEventListener("MozOrientation", update_accelerometer, true);
 
 // Mobile Safari
-//window.addEventListener("devicemotion", update_accelerometer, true);
+window.addEventListener("devicemotion", update_accelerometer, true);
 
 
 function update_accelerometer(e){
     // gets the accelerometer vals and passes them back
 	if(!!e.x) {
 		// Firefox
-		var x = e.x*90;
-		var y = e.y*90;
-		var z = 0;
+		var x = Math.round(e.x*90);
+		var y = Math.round(e.y*90);
+		//var z = 0;
 		update_text(x, y, z); // no compass value for Firefox :( you can get z accel though
-		socket.send({room: room, action: 'movement', x: x, y: y, z: z});
+		socket.send({room: room, action: 'movement', x: x, y: y, z: z, method: 'orientation'});
 		
 	} else if (!!e.accelerationIncludingGravity){
 		// Mobile Safari
-		update_text(e.gamma, e.beta, e.alpha);	
+		var x = Math.round(e.gamma);
+		var y = Math.round(e.beta);
+		z = Math.round(e.alpha);
+		update_text(x, y, z);
+		socket.send({room: room, action: 'movement', x: x, y: y, z: z, method: 'orientation'});
+		
 	} else {
 		//alert("you don't really support devicemotion, do you?");
 		;
@@ -43,14 +49,15 @@ $(function() {
 	// socket tester.
 	$("#test").click (function () {
 		socket.send({room: room, action: 'test'});
+		z++;
 	});
 	
 	// this is the browser test version
 	$(window).bind("mousemove", function(e) {
 		var x = e.pageX;
 		var y = e.pageY;
-		socket.send({room: room, action: 'movement', x: x, y: y, z: 0});
-		update_text(x, y, 0);
+		socket.send({room: room, action: 'movement', x: x, y: y, z: z, method: 'mouse'});
+		update_text(x, y, z);
 	});
 	
 	
