866cac0351a223efad0e8908a815b265c93bac59
= <ajfisher.td@gmail.com>
2011-08-19 21:53:09 +1000

    refactored a bunch of URLS

--- arduino_socket/urls.py
+++ arduino_socket/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls.defaults import patterns, include, url
 
 
 urlpatterns = patterns('',
-    url("^$", "puppet.views.home"),
-    url("^drive", "puppet.views.drive"),
+    url("^$", "light.views.home"),
+    url("^drive", include('light.urls')),
+    url("^axis", include('axis.urls')),
     url("", include("django_socketio.urls")),
 )
